bdbf3c8dc03d20dff8d8a31bea6100c4b588051f
Florian Bussmann <hallo@florian-bussmann.de>
2025-08-12 18:52:24 +0000

    feat: extract info (categories and tags)

--- src/routes.ts
+++ src/routes.ts
@@ -13,11 +13,35 @@ router.addDefaultHandler(async ({ enqueueLinks, request, log }) => {
 });
 
 router.addHandler('detail', async ({ request, page, log, pushData }) => {
+    log.info(`Processing ${request.url}...`);
+
     const title = await page.title();
     log.info(`${title}`, { url: request.loadedUrl });
+
+    const info = await page.$$eval('div.info > div.item', (items) => {
+        const result: { categories: string[]; tags: string[] } = {
+            categories: [],
+            tags: [],
+        };
+
+        items.forEach(item => {
+            const label = item.childNodes[0].textContent?.trim().replace(':', '') || '';
+
+            const links = Array.from(item.querySelectorAll('a')).map(a => a.textContent?.trim()).filter(Boolean);
+
+            if (label.toLowerCase() === 'categories') {
+                result.categories = links;
+            } else if (label.toLowerCase() === 'tags') {
+                result.tags = links;
+            }
+        });
+
+        return result;
+    });
 
     await pushData({
         url: request.loadedUrl,
         title,
+        info,
     });
 });
